9c17468f3f28a3f27a5043a78ed38e5ee2bae66d
bensmith-314 <ben@tiika.co>
2024-08-26 18:53:31 -0600

    Updated for reverse chronological order of files

--- everydayGenerator.py
+++ everydayGenerator.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 
-directory = Path('everydays')
+directory = Path('everydays_small')
 
 # List all files in the directory and sort them alphabetically by name
 files = sorted([f for f in directory.iterdir() if f.is_file()])
 
 # Remove .DS_Store
 files = files[1:]
+files.reverse()
 
 with open("imagePaths.txt", "w") as newFile:
     for file in files:
