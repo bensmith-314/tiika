5ad60d32d17557cfce0337581b2322e6f6c5dccc
bensmith-314 <ben@tiika.co>
2026-02-06 23:40:52 -0700

    Update max value of random number

--- js/randomArt.js
+++ js/randomArt.js
@@ -1,6 +1,6 @@
 function randomArtPiece() {
-    const randomIndex = Math.floor(Math.random() * 1533); // Needs to be dynamically updated when generated
-    window.location.href = `${randomIndex}.html`;
+    const randomIndex = Math.floor(Math.random() * 1782); // Needs to be dynamically updated when generated
+    window.location.href = `${randomIndex}`;
 }
 
 // Attach event listener to the button
