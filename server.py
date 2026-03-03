19ae7353318f1c0050ef5405188cb2ef323bcb47
bensmith-314 <ben@tiika.co>
2026-03-03 13:21:35 -0700

    Enable URL Rewriting

--- server.py
+++ server.py
@@ -1,20 +1,33 @@
 import http.server
 import socketserver
 import os
+from pathlib import Path
 
 PORT = 8000
-DIRECTORY = "."
+DIRECTORY = "/Users/bensmith/Desktop/tiika"
 
-socketserver.TCPServer.allow_reuse_address = True
-
-class CustomHandler(http.server.SimpleHTTPRequestHandler):
+class RewriteHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+    
     def do_GET(self):
-        if self.path == '/':
-            self.path = '/index.html'  # Specify your custom start page here
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+        # Get the requested path
+        path = self.translate_path(self.path)
+        
+        # If path doesn't exist and doesn't end in .html, try adding .html
+        if not os.path.exists(path) and not self.path.endswith('.html'):
+            # Remove query string if present
+            clean_path = self.path.split('?')[0]
+            html_path = self.translate_path(clean_path + '.html')
+            
+            if os.path.exists(html_path):
+                # Rewrite the path to include .html
+                self.path = clean_path + '.html'
+        
+        # Call the parent class to handle the request normally
+        return super().do_GET()
 
-os.chdir(DIRECTORY)
-
-with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
-    print("Serving at port", PORT)
+with socketserver.TCPServer(("", PORT), RewriteHTTPRequestHandler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    print(f"Directory: {DIRECTORY}")
     httpd.serve_forever()
